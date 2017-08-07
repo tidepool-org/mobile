e98115eef59190fc13b6bf168b02de696301d331
Mark Young <markyou@gmail.com>
2017-08-07 12:18:37 -0500

    Eject to ExpoKit

---
+++ expo/android/app/src/main/java/org/tidepool/expo_blip_notes/MainActivity.java
@@ -0,0 +1,46 @@
+package org.tidepool.expo_blip_notes;
+
+import android.os.Bundle;
+
+import com.facebook.react.ReactPackage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.tidepool.expo_blip_notes.generated.ExponentBuildConstants;
+import host.exp.expoview.ExponentActivity;
+
+public class MainActivity extends ExponentActivity {
+
+  @Override
+  public String publishedUrl() {
+    return "exp://exp.host/@markyou/tidepool-mobile";
+  }
+
+  @Override
+  public String developmentUrl() {
+    return ExponentBuildConstants.DEVELOPMENT_URL;
+  }
+
+  @Override
+  public List<String> sdkVersions() {
+    return new ArrayList<>(Arrays.asList("19.0.0"));
+  }
+
+  @Override
+  public List<ReactPackage> reactPackages() {
+    return ((MainApplication) getApplication()).getPackages();
+  }
+
+  @Override
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
+
+  @Override
+  public Bundle initialProps(Bundle expBundle) {
+    // Add extra initialProps here
+    return expBundle;
+  }
+}
---
+++ expo/android/app/src/main/java/org/tidepool/expo_blip_notes/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package org.tidepool.expo_blip_notes.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "";
+
+}
