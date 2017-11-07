d4b4ec9d08f3cb08668893c18d4338cfc76ad36b
Mark Young <mark@dufflite.com>
2017-11-06 20:10:05 -0800

    Update storybook, react, react native, expo

--- expo/storybook/storyLoader.js
+++ expo/storybook/storyLoader.js
@@ -5,7 +5,6 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  
   require('../src/components/Button/__stories__/ButtonStory.js');
   require('../src/components/EventList/__stories__/EventListStory.js');
   require('../src/components/MadePossibleBy/__stories__/SignUpStory.js');
@@ -17,6 +16,19 @@ function loadStories() {
   
 }
 
+const stories = [
+  '../src/components/Button/__stories__/ButtonStory.js',
+  '../src/components/EventList/__stories__/EventListStory.js',
+  '../src/components/MadePossibleBy/__stories__/SignUpStory.js',
+  '../src/components/NoteListItem/__stories__/NoteListItemStory.js',
+  '../src/components/SignInForm/__stories__/SignInForm.js',
+  '../src/components/SignUp/__stories__/SignUpStory.js',
+  '../src/components/VersionAndEnvironment/__stories__/VersionAndEnvironmentStory.js',
+  '../src/screens/SignInScreen/__stories__/SignInScreenStory.js',
+  
+];
+
 module.exports = {
   loadStories,
+  stories,
 };
