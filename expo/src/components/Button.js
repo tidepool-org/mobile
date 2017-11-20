43007179d7425ceb98e982e9cc4b93f359f99e8e
Mark Young <mark@dufflite.com>
2017-11-20 17:29:50 -0600

    Button fixes
    - Fix issue with Android button tap handler not responding to taps in release build
    - Use TouchableOpacity for now (instead of TouchableNativeFeedback) for consistency in look/feel. NB: This also fixes the tap onPress issue. Will need to revisit that if we switch back to TouchableNativeFeedback

--- expo/src/components/Button.js
+++ expo/src/components/Button.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Platform, StyleSheet, TouchableNativeFeedback } from "react-native";
-import glamorous, { withTheme } from "glamorous-native";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 
 const styles = StyleSheet.create({
   button: {
-    paddingLeft: 8,
-    paddingRight: 8,
+    paddingLeft: 16,
+    paddingRight: 16,
     elevation: 4,
     backgroundColor: "#627cff",
     borderRadius: 3,
@@ -27,38 +32,45 @@ const styles = StyleSheet.create({
   },
 });
 
-const Button = ({ title, color, disabled, onPress }) => {
-  const buttonStyles = [styles.button];
-  const textStyles = [styles.text];
-  if (color) {
-    buttonStyles.push({ backgroundColor: color });
+class Button extends Component {
+  render() {
+    const { color, onPress, title, disabled } = this.props;
+    const buttonStyles = [styles.button];
+    const textStyles = [styles.text];
+    if (color) {
+      if (Platform.OS === "ios") {
+        textStyles.push({ color });
+      } else {
+        buttonStyles.push({ backgroundColor: color });
+      }
+    }
+    const accessibilityTraits = ["button"];
+    if (disabled) {
+      buttonStyles.push(styles.buttonDisabled);
+      textStyles.push(styles.textDisabled);
+      accessibilityTraits.push("disabled");
+    }
+    const formattedTitle =
+      Platform.OS === "android" ? title.toUpperCase() : title;
+    const Touchable = TouchableOpacity;
+    // TODO: Revisit this. Do we want native feedback for Android? Or consistency/parity between iOS and Android
+    // const Touchable =
+    //   Platform.OS === "android" ? TouchableNativeFeedback : TouchableOpacity;
+    return (
+      <Touchable
+        accessibilityComponentType="button"
+        disabled={disabled}
+        onPress={onPress}
+      >
+        <View style={buttonStyles}>
+          <Text style={textStyles} disabled={disabled}>
+            {formattedTitle}
+          </Text>
+        </View>
+      </Touchable>
+    );
   }
-  const accessibilityTraits = ["button"];
-  if (disabled) {
-    buttonStyles.push(styles.buttonDisabled);
-    textStyles.push(styles.textDisabled);
-    accessibilityTraits.push("disabled");
-  }
-  const formattedTitle =
-    Platform.OS === "android" ? title.toUpperCase() : title;
-  const Touchable =
-    Platform.OS === "android"
-      ? TouchableNativeFeedback
-      : glamorous.TouchableOpacity;
-  return (
-    <Touchable
-      accessibilityComponentType="button"
-      disabled={disabled}
-      onPress={onPress}
-    >
-      <glamorous.View style={buttonStyles}>
-        <glamorous.Text style={textStyles} disabled={disabled}>
-          {formattedTitle}
-        </glamorous.Text>
-      </glamorous.View>
-    </Touchable>
-  );
-};
+}
 
 Button.propTypes = {
   title: PropTypes.string.isRequired,
@@ -72,4 +84,4 @@ Button.defaultProps = {
   disabled: false,
 };
 
-export default withTheme(Button);
+export default Button;
