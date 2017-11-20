6467650bf3b3db34ee77ad0628fa5651e84212d9
Mark Young <mark@dufflite.com>
2017-11-20 17:28:00 -0600

    Remove some cruft comments

--- expo/__stories__/components/SignInForm.story.js
+++ expo/__stories__/components/SignInForm.story.js
@@ -4,10 +4,6 @@ import { storiesOf } from "@storybook/react-native";
 import { StoryContainerComponent } from "../../__stories__/utils/StoryContainer";
 import SignInForm from "../../src/components/SignInForm";
 
-// TODO: my - 0 - maybe move the stories and tests to top-level to avoid clutter for components?? also then removes that stupid index.js stuff
-// TODO: my - 0 - can tests also be moved that high? We should try that, not sure if CRNA requires tests in src/ or not
-// TODO: my - 0 - before committing this current set of changes, confirm we can do both stories and tests outside of src/ without issue
-
 storiesOf("SignInForm", module).add("default", () => (
   <StoryContainerComponent>
     <SignInForm />
