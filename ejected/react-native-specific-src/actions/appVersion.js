0cdf161d266f21adb04b3d0a019804bb2c580e1e
Mark Young <markyou@gmail.com>
2017-12-19 22:58:50 -0600

    Use react-native-version-number to get app version

--- ejected/react-native-specific-src/actions/appVersion.js
+++ ejected/react-native-specific-src/actions/appVersion.js
@@ -1,9 +1,15 @@
 // NOTE: This file is copied via build script to src/actions/appVersion.js
 
+import VersionNumber from "react-native-version-number";
+
 export const APP_VERSION_SET_VERSION = "APP_VERSION_SET_VERSION";
 
 // TODO: app version - use react-native-device-info
-export const appVersionSetVersion = () => ({
-  type: APP_VERSION_SET_VERSION,
-  payload: "3.0 (React Native)",
-});
+export const appVersionSetVersion = () => {
+  const version = `${VersionNumber.appVersion}`;
+
+  return {
+    type: APP_VERSION_SET_VERSION,
+    payload: version,
+  };
+};
