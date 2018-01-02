2c17083242540d5459b2052af56e757564f345e3
Mark Young <mark@dufflite.com>
2018-01-01 21:41:36 -0600

    Update npm dependencies for expo project
    - Update npm dependencies
    - Fix breaking change in latest React Navigation
    - Update storyLoader

--- expo/storybook/storyLoader.js
+++ expo/storybook/storyLoader.js
@@ -5,40 +5,40 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  require('../__stories__/components/Button.story.js');
-  require('../__stories__/components/DebugSettings.story.js');
-  require('../__stories__/components/Drawer.story.js');
-  require('../__stories__/components/HashtagText.story.js');
-  require('../__stories__/components/HomeScreen.story.js');
-  require('../__stories__/components/MadePossible.story.js');
-  require('../__stories__/components/NotesList.story.js');
-  require('../__stories__/components/NotesListItem.story.js');
-  require('../__stories__/components/NotesListItemAddComment.story.js');
-  require('../__stories__/components/NotesListItemComment.story.js');
-  require('../__stories__/components/ProfileList.story.js');
-  require('../__stories__/components/SignInForm.story.js');
-  require('../__stories__/components/SignInScreen.story.js');
-  require('../__stories__/components/SwitchProfileScreen.story.js');
-  require('../__stories__/components/VersionAndApiEnvironment.story.js');
+  require('../__stories__/components/Button.story');
+  require('../__stories__/components/DebugSettings.story');
+  require('../__stories__/components/Drawer.story');
+  require('../__stories__/components/HashtagText.story');
+  require('../__stories__/components/HomeScreen.story');
+  require('../__stories__/components/MadePossible.story');
+  require('../__stories__/components/NotesList.story');
+  require('../__stories__/components/NotesListItem.story');
+  require('../__stories__/components/NotesListItemAddComment.story');
+  require('../__stories__/components/NotesListItemComment.story');
+  require('../__stories__/components/ProfileList.story');
+  require('../__stories__/components/SignInForm.story');
+  require('../__stories__/components/SignInScreen.story');
+  require('../__stories__/components/SwitchProfileScreen.story');
+  require('../__stories__/components/VersionAndApiEnvironment.story');
   
 }
 
 const stories = [
-  '../__stories__/components/Button.story.js',
-  '../__stories__/components/DebugSettings.story.js',
-  '../__stories__/components/Drawer.story.js',
-  '../__stories__/components/HashtagText.story.js',
-  '../__stories__/components/HomeScreen.story.js',
-  '../__stories__/components/MadePossible.story.js',
-  '../__stories__/components/NotesList.story.js',
-  '../__stories__/components/NotesListItem.story.js',
-  '../__stories__/components/NotesListItemAddComment.story.js',
-  '../__stories__/components/NotesListItemComment.story.js',
-  '../__stories__/components/ProfileList.story.js',
-  '../__stories__/components/SignInForm.story.js',
-  '../__stories__/components/SignInScreen.story.js',
-  '../__stories__/components/SwitchProfileScreen.story.js',
-  '../__stories__/components/VersionAndApiEnvironment.story.js',
+  '../__stories__/components/Button.story',
+  '../__stories__/components/DebugSettings.story',
+  '../__stories__/components/Drawer.story',
+  '../__stories__/components/HashtagText.story',
+  '../__stories__/components/HomeScreen.story',
+  '../__stories__/components/MadePossible.story',
+  '../__stories__/components/NotesList.story',
+  '../__stories__/components/NotesListItem.story',
+  '../__stories__/components/NotesListItemAddComment.story',
+  '../__stories__/components/NotesListItemComment.story',
+  '../__stories__/components/ProfileList.story',
+  '../__stories__/components/SignInForm.story',
+  '../__stories__/components/SignInScreen.story',
+  '../__stories__/components/SwitchProfileScreen.story',
+  '../__stories__/components/VersionAndApiEnvironment.story',
   
 ];
 
