65c3267294c6783fbca0f1cc272e3176fd105f9a
Mark Young <markyou@gmail.com>
2018-05-18 19:50:13 -0500

    Add app version support that works with both Expo and ExpoKit (ejected) - Add react-native-device npm module - Add compatible googlePlayServicesVersion to ext in android build.gradle (for react-native-device) - Update appVersion.js action to use react-native-device - Remove the old Expo-specific appVersion (no longer needed, since we're using same appVersion for both Expo and ExpoKit (ejected))

--- expo/ios/Pods/Target Support Files/Pods-tidepool/Pods-tidepool-dummy.m
+++ expo/ios/Pods/Target Support Files/Pods-tidepool/Pods-tidepool-dummy.m
@@ -1,5 +1,5 @@
 #import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tidepool : NSObject
+@interface PodsDummy_Pods_Tidepool : NSObject
 @end
-@implementation PodsDummy_Pods_tidepool
+@implementation PodsDummy_Pods_Tidepool
 @end
