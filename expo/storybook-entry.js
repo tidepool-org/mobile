2212376c4e2755325442d8d9955ce711a559c1bd
Mark Young <markyou@gmail.com>
2018-05-21 10:27:37 -0500

    Update storybook init to pre-load assets for three-bmfont-text rendering

--- expo/storybook-entry.js
+++ expo/storybook-entry.js
@@ -3,9 +3,16 @@ import Expo from "expo";
 import Fonts from "./src/constants/Fonts";
 import withExpoFontPreload from "./src/enhancers/withExpoFontPreload";
 import App from "./storybook";
+import GraphTextMeshFactory from "./src/components/Graph/gl/GraphTextMeshFactory";
 
 if (process.env.NODE_ENV === "development") {
   Expo.KeepAwake.activate();
 }
+
+const loadAssets = async () => {
+  await GraphTextMeshFactory.loadAssets();
+};
+
+loadAssets();
 
 Expo.registerRootComponent(withExpoFontPreload(App, Fonts));
