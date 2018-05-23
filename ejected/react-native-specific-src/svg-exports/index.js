7b6d65f40d827feab53d1410017d7d5269b4ddfa
Mark Young <mark@dufflite.com>
2018-05-23 16:56:58 -0500

    Remove the old (obsolete) ejected folder. Move the expo folder contents to root.

--- ejected/react-native-specific-src/svg-exports/index.js
+++
@@ -1,5 +0,0 @@
-// This svg-exports/index.js is a helper to deal with difference between Expo and react-native-svg
-// in how the various Svg elements are exported. The correct version (for either expo or ejected
-// app) is copied into place via build script.
-
-export * from "react-native-svg";
--- ejected/react-native-specific-src/utils/runtimeEnvironment.js
+++
@@ -1,7 +0,0 @@
-// NOTE: This file is copied via build script to src/utils/runtimeEnvironment.js
-
-const environment = {
-  useExpo: false,
-};
-
-export default environment;
