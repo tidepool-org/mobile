359b49bca1b3c9f6e3d8bb7870f86d595f162e0b
Mark Young <mark@dufflite.com>
2018-06-01 12:51:30 -0500

    Add support for auto-scrollable/scalable objects in Graph layers to DRY things up (before adding basal, bolus, wizard layers)
    - Add support for adding auto-scrollable/scalable objects to the scene (during initial render) and auto-updating the position and scale auto-scrollable/scalable objects in scene (during scroll/zoom)
    - Split the graph render render into a base render and a renderSelf. The base handles whether to draw or not (based on whether data exists), whether it's the initial draw, or whether it's a draw for scroll or zoom. It will also now automatically handle updates to auto-scrollable/scalable objects during scroll and zoom.
    - Update existing graph layers to implement hasDataToRender (to indicate whether data exists to render) and renderSelf. In renderSelf, add auto-scrollable objects to scene, if it's the initial render. Remove now unneeded functionality in existing graph layers for handling updating objects during scroll and zoom.

--- src/components/Graph/gl/GraphRenderLayerGl.js
+++ src/components/Graph/gl/GraphRenderLayerGl.js
@@ -13,8 +13,69 @@ class GraphRenderLayerGl {
     this.graphStartTimeSeconds = graphStartTimeSeconds;
     this.zStart = zStart;
     this.zEnd = zEnd;
-
+    this.scrollableObjectsStartIndex = undefined;
     this.pixelRatio = PixelRatio.get();
+    this.isInitialRender = true;
+    this.isScrollOrZoomRender = false;
+  }
+
+  addAutoScrollableObjectToScene(
+    scene,
+    object,
+    { x, y, z, contentOffsetX, pixelsPerSecond, shouldScaleX = false }
+  ) {
+    const objectToTrack = object;
+
+    // Remember the start index for scrollable objects
+    // NOTE: All scrollabe objects need to be added contiguously in initial render pass
+    if (this.scrollableObjectsStartIndex === undefined) {
+      this.scrollableObjectsStartIndex = scene.children.length;
+      this.scrollableObjectCount = 0;
+    }
+
+    // Set the initial position (and scale)
+    objectToTrack.userData = {
+      x,
+      y,
+      z,
+      shouldScaleX,
+    };
+    this.updateScrollableObject(objectToTrack, {
+      contentOffsetX,
+      pixelsPerSecond,
+    });
+
+    // Add it to the scene and track it
+    scene.add(objectToTrack);
+    this.scrollableObjectCount += 1;
+  }
+
+  updateAutoScrollableObjectsInScene(
+    scene,
+    { contentOffsetX, pixelsPerSecond }
+  ) {
+    const objects = scene.children;
+    const startIndex = this.scrollableObjectsStartIndex;
+    const endIndex =
+      this.scrollableObjectsStartIndex + this.scrollableObjectCount - 1;
+    for (let i = startIndex; i <= endIndex; i += 1) {
+      const object = objects[i];
+      this.updateScrollableObject(object, {
+        contentOffsetX,
+        pixelsPerSecond,
+      });
+    }
+  }
+
+  updateScrollableObject(object, { contentOffsetX, pixelsPerSecond }) {
+    if (object.userData.shouldScaleX) {
+      object.scale.set(pixelsPerSecond, 1, 1);
+    }
+    this.updateObjectPosition(object, {
+      x: object.userData.x * pixelsPerSecond - contentOffsetX,
+      y: object.userData.y,
+      z: object.userData.z,
+    });
   }
 
   updateObjectPosition(object, { x, y, z }) {
@@ -25,6 +86,34 @@ class GraphRenderLayerGl {
     object.position.y = -y * this.pixelRatio;
     object.position.z = z;
     /* eslint-enable no-param-reassign */
+  }
+
+  render({ scene, graphScalableLayoutInfo, contentOffsetX, ...rest }) {
+    if (!this.hasDataToRender || this.hasDataToRender({ ...rest })) {
+      if (!this.isInitialRender) {
+        this.isScrollOrZoomRender = true;
+      }
+
+      if (this.renderSelf) {
+        this.renderSelf({
+          scene,
+          graphScalableLayoutInfo,
+          contentOffsetX,
+          ...rest,
+        });
+      }
+
+      if (this.isScrollOrZoomRender) {
+        // Automatically update auto-scrollable objects in scene if rendering for scroll or zoom
+        const { pixelsPerSecond } = graphScalableLayoutInfo;
+        this.updateAutoScrollableObjectsInScene(scene, {
+          contentOffsetX,
+          pixelsPerSecond,
+        });
+      }
+
+      this.isInitialRender = false;
+    }
   }
 }
 
