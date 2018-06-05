e1bf1eef4547c4518d4fc519e577fae1f3b2518a
Mark Young <markyou@gmail.com>
2018-06-05 07:52:46 -0500

    Graph - Add sprite-based rendering for CBG/SMBG and outlines for SMBG - Add support for sprite-based rendering for CBG/SMBG circles. Also still support circle geometry. Currently still using circle geometry (better fidelity so-far). - Add outlines to SMBG circles for circle geometry rendering (sprites have that baked in). This is similar to old iOS Tidepool Mobile app that had outlines for the SMBG circles, using the graph background color, to provide some visual separation so when multiple are close together it doesn't look like a blog. This way you can still see two circles.

--- src/components/Graph/gl/GraphCbgGl.js
+++ src/components/Graph/gl/GraphCbgGl.js
@@ -1,27 +1,80 @@
-import { THREE } from "expo-three";
+import ExpoTHREE, { THREE } from "expo-three";
 
 import GraphRenderLayerGl from "./GraphRenderLayerGl";
 import { MAX_BG_VALUE, convertHexColorStringToInt } from "../helpers";
 
+const { createTextureAsync } = ExpoTHREE;
+
+// Support rendering using circle geometry or sprites
+const useSprites = false;
+
 class GraphCbgGl extends GraphRenderLayerGl {
   constructor(props) {
-    super(props);
-
     // console.log(`GraphCbgGl ctor`);
 
-    this.circleGeometry = new THREE.CircleBufferGeometry(
-      3.5 * this.pixelRatio,
-      8 * this.pixelRatio
-    );
-    this.lowMaterial = new THREE.MeshBasicMaterial({
-      color: convertHexColorStringToInt(this.theme.graphBgLowColor),
-    });
-    this.normalMaterial = new THREE.MeshBasicMaterial({
-      color: convertHexColorStringToInt(this.theme.graphBgNormalColor),
-    });
-    this.highMaterial = new THREE.MeshBasicMaterial({
-      color: convertHexColorStringToInt(this.theme.graphBgHighColor),
-    });
+    super(props);
+
+    const radius = 3.5;
+    if (useSprites) {
+      this.lowSpriteMaterial = new THREE.MeshBasicMaterial({
+        map: GraphCbgGl.lowSpriteTexture,
+        transparent: true,
+      });
+      this.normalSpriteMaterial = new THREE.MeshBasicMaterial({
+        map: GraphCbgGl.normalSpriteTexture,
+        transparent: true,
+      });
+      this.highSpriteMaterial = new THREE.MeshBasicMaterial({
+        map: GraphCbgGl.highSpriteTexture,
+        transparent: true,
+      });
+      this.spriteGeometry = new THREE.PlaneGeometry(
+        radius * 2 * this.pixelRatio,
+        radius * 2 * this.pixelRatio,
+        1,
+        1
+      );
+    } else {
+      this.circleGeometry = new THREE.CircleBufferGeometry(
+        radius * this.pixelRatio,
+        8 * this.pixelRatio
+      );
+      this.lowMaterial = new THREE.MeshBasicMaterial({
+        color: convertHexColorStringToInt(this.theme.graphBgLowColor),
+      });
+      this.normalMaterial = new THREE.MeshBasicMaterial({
+        color: convertHexColorStringToInt(this.theme.graphBgNormalColor),
+      });
+      this.highMaterial = new THREE.MeshBasicMaterial({
+        color: convertHexColorStringToInt(this.theme.graphBgHighColor),
+      });
+    }
+  }
+
+  static async loadAssets() {
+    if (GraphCbgGl.assetsAreLoaded) {
+      return;
+    }
+
+    if (useSprites) {
+      GraphCbgGl.lowSpriteTexture = await createTextureAsync({
+        asset: require("../../../../assets/images/cbg-circle-low.png"),
+      });
+      GraphCbgGl.lowSpriteTexture.magFilter = THREE.NearestFilter;
+      GraphCbgGl.lowSpriteTexture.minFilter = THREE.NearestFilter;
+      GraphCbgGl.normalSpriteTexture = await createTextureAsync({
+        asset: require("../../../../assets/images/cbg-circle-normal.png"),
+      });
+      GraphCbgGl.normalSpriteTexture.magFilter = THREE.NearestFilter;
+      GraphCbgGl.normalSpriteTexture.minFilter = THREE.NearestFilter;
+      GraphCbgGl.highSpriteTexture = await createTextureAsync({
+        asset: require("../../../../assets/images/cbg-circle-high.png"),
+      });
+      GraphCbgGl.highSpriteTexture.magFilter = THREE.NearestFilter;
+      GraphCbgGl.highSpriteTexture.minFilter = THREE.NearestFilter;
+    }
+
+    GraphCbgGl.assetsAreLoaded = true;
   }
 
   /* eslint-disable class-methods-use-this */
@@ -29,6 +82,60 @@ class GraphCbgGl extends GraphRenderLayerGl {
     return cbgData && cbgData.length > 0;
   }
   /* eslint-enable class-methods-use-this */
+
+  renderSelfUsingCircleGeometry({
+    scene,
+    contentOffsetX,
+    pixelsPerSecond,
+    x,
+    y,
+    z,
+    isLow,
+    isHigh,
+  }) {
+    let material = this.normalMaterial;
+    if (isLow) {
+      material = this.lowMaterial;
+    } else if (isHigh) {
+      material = this.highMaterial;
+    }
+    const object = new THREE.Mesh(this.circleGeometry, material);
+    this.addAutoScrollableObjectToScene(scene, object, {
+      x,
+      y,
+      z,
+      contentOffsetX,
+      pixelsPerSecond,
+      shouldScrollX: true,
+    });
+  }
+
+  renderSelfUsingSprites({
+    scene,
+    contentOffsetX,
+    pixelsPerSecond,
+    x,
+    y,
+    z,
+    isLow,
+    isHigh,
+  }) {
+    let material = this.normalSpriteMaterial;
+    if (isLow) {
+      material = this.lowSpriteMaterial;
+    } else if (isHigh) {
+      material = this.highSpriteMaterial;
+    }
+    const object = new THREE.Mesh(this.spriteGeometry, material);
+    this.addAutoScrollableObjectToScene(scene, object, {
+      x,
+      y,
+      z,
+      contentOffsetX,
+      pixelsPerSecond,
+      shouldScrollX: true,
+    });
+  }
 
   renderSelf({ scene, graphScalableLayoutInfo, contentOffsetX, cbgData }) {
     // console.log(`GraphCbgGl renderSelf`);
@@ -53,22 +160,31 @@ class GraphCbgGl extends GraphRenderLayerGl {
           this.graphFixedLayoutInfo.yAxisBottomOfGlucose -
           constrainedValue *
             this.graphFixedLayoutInfo.yAxisGlucosePixelsPerValue;
+        const z = this.zStart + i * 0.01;
 
-        let material = this.normalMaterial;
-        if (isLow) {
-          material = this.lowMaterial;
-        } else if (isHigh) {
-          material = this.highMaterial;
+        if (useSprites) {
+          this.renderSelfUsingSprites({
+            scene,
+            contentOffsetX,
+            pixelsPerSecond,
+            x,
+            y,
+            z,
+            isLow,
+            isHigh,
+          });
+        } else {
+          this.renderSelfUsingCircleGeometry({
+            scene,
+            contentOffsetX,
+            pixelsPerSecond,
+            x,
+            y,
+            z,
+            isLow,
+            isHigh,
+          });
         }
-        const object = new THREE.Mesh(this.circleGeometry, material);
-        this.addAutoScrollableObjectToScene(scene, object, {
-          x,
-          y,
-          z: this.zStart + i * 0.01,
-          contentOffsetX,
-          pixelsPerSecond,
-          shouldScrollX: true,
-        });
       }
     }
   }
