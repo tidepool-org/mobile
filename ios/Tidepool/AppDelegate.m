8abf1f9fd59a692ecff9298a9d72e43fc7fcce09
Mark Young <mark@dufflite.com>
2019-04-13 22:29:40 -0500

    Update to Expo 32 and update most other dependencies. A commit from 'pod install' will follow this commit, to make the PR easier to review.
    See: "[RN] Remove unused Branch.io support to avoid Google Play rejection. Update to latest Expo 32, update other dependencies"

--- ios/Tidepool/AppDelegate.m
+++ ios/Tidepool/AppDelegate.m
@@ -19,56 +19,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    BOOL result = [super application:application didFinishLaunchingWithOptions:launchOptions];
+
     [RollbarReactNative initWithAccessToken:@"00788919100a467e8fb08144b427890e"];
 
-    _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    _window.backgroundColor = [UIColor whiteColor];
-    [[ExpoKit sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
-    _rootViewController = [ExpoKit sharedInstance].rootViewController;
-    _window.rootViewController = _rootViewController;
-
-    [_window makeKeyAndVisible];
-
-    return YES;
-}
-
-#pragma mark - Handling URLs
-
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(nullable NSString *)sourceApplication annotation:(id)annotation
-{
-    return [[ExpoKit sharedInstance] application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
-}
-
-- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray * _Nullable))restorationHandler
-{
-    return [[ExpoKit sharedInstance] application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
-}
-
-#pragma mark - Notifications
-
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)token
-{
-    [[ExpoKit sharedInstance] application:application didRegisterForRemoteNotificationsWithDeviceToken:token];
-}
-
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)err
-{
-    [[ExpoKit sharedInstance] application:application didFailToRegisterForRemoteNotificationsWithError:err];
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)notification
-{
-    [[ExpoKit sharedInstance] application:application didReceiveRemoteNotification:notification];
-}
-
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(nonnull UILocalNotification *)notification
-{
-    [[ExpoKit sharedInstance] application:application didReceiveLocalNotification:notification];
-}
-
-- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(nonnull UIUserNotificationSettings *)notificationSettings
-{
-    [[ExpoKit sharedInstance] application:application didRegisterUserNotificationSettings:notificationSettings];
+    return result;
 }
 
 #pragma mark - NativeNotifications
