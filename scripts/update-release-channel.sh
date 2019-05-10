5975eac9a8e6b1e6fbfdf55d75fc855813e0bdd6
Mark Young <mark@dufflite.com>
2019-05-10 12:29:29 -0500

    Attempt to fix Travis CI Android build

--- scripts/update-release-channel.sh
+++ scripts/update-release-channel.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+set -o nounset
+
 # Update manifests to use specified release channel rather than default
-pwd
-sed -i '.bak' "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\"$1\"/" android/app/src/main/assets/shell-app-manifest.json
-sed -i '.bak' "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\"$1\"/" android/app/src/main/assets/kernel-manifest.json
-sed -i '.bak' "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\"$1\"/" ios/Tidepool/Supporting/shell-app-manifest.json
-sed -i '.bak' "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\"$1\"/" ios/Tidepool/Supporting/EXShell.json
-sed -i '.bak' "s/\<string\>default\<\/string\>/\<string\>$1\<\/string\>/" ios/Tidepool/Supporting/EXShell.plist
+pwd # remove this later, just testing Travis CI
+sed -i .bak "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\"$1\"/" android/app/src/main/assets/shell-app-manifest.json
+sed -i .bak "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\"$1\"/" android/app/src/main/assets/kernel-manifest.json
+sed -i .bak "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\"$1\"/" ios/Tidepool/Supporting/shell-app-manifest.json
+sed -i .bak "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\"$1\"/" ios/Tidepool/Supporting/EXShell.json
+sed -i .bak "s/\<string\>default\<\/string\>/\<string\>$1\<\/string\>/" ios/Tidepool/Supporting/EXShell.plist
+cat ios/Tidepool/Supporting/EXShell.plist # remove this later, just testing Travis CI
