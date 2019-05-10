d5e4a6b0109cbb32d2727e546c872907601e718f
Mark Young <mark@dufflite.com>
2019-05-10 13:40:59 -0500

    Attempt to fix Travis build

--- scripts/update-release-channel.sh
+++ scripts/update-release-channel.sh
@@ -3,19 +3,20 @@
 set -o nounset
 
 # Update manifests to use specified release channel rather than default
-pwd # remove this later, just testing Travis CI
 
-if [ "$(uname)" == "Darwin" ]; then
-    sed -i '' "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$1"\"/" android/app/src/main/assets/shell-app-manifest.json
-    sed -i '' "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$1"\"/" android/app/src/main/assets/kernel-manifest.json
-    sed -i '' "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$1"\"/" ios/Tidepool/Supporting/shell-app-manifest.json
-    sed -i '' "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$1"\"/" ios/Tidepool/Supporting/EXShell.json
-    sed -i '' "s/\<string\>default\<\/string\>/\<string\>"$1"\<\/string\>/" ios/Tidepool/Supporting/EXShell.plist
-else
-    sed -i "s/\<string\>default\<\/string\>/\<string\>"$1"\<\/string\>/" ios/Tidepool/Supporting/EXShell.plist
-    sed -i "s/\<string\>default\<\/string\>/\<string\>"$1"\<\/string\>/" ios/Tidepool/Supporting/EXShell.plist
-    sed -i "s/\<string\>default\<\/string\>/\<string\>"$1"\<\/string\>/" ios/Tidepool/Supporting/EXShell.plist
-    sed -i "s/\<string\>default\<\/string\>/\<string\>"$1"\<\/string\>/" ios/Tidepool/Supporting/EXShell.plist
-    sed -i "s/\<string\>default\<\/string\>/\<string\>"$1"\<\/string\>/" ios/Tidepool/Supporting/EXShell.plist
-fi
+target_file=android/app/src/main/assets/shell-app-manifest.json
+sed -e "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$1"\"/" "$target_file" > "$target_file.$$" && mv "$target_file.$$" "$target_file"
+
+target_file=android/app/src/main/assets/kernel-manifest.json
+sed -e "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$1"\"/" "$target_file" > "$target_file.$$" && mv "$target_file.$$" "$target_file"
+
+target_file=ios/Tidepool/Supporting/shell-app-manifest.json
+sed -e "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$1"\"/" "$target_file" > "$target_file.$$" && mv "$target_file.$$" "$target_file"
+
+target_file=ios/Tidepool/Supporting/EXShell.json
+sed -e "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$1"\"/" "$target_file" > "$target_file.$$" && mv "$target_file.$$" "$target_file"
+
+target_file=ios/Tidepool/Supporting/EXShell.plist
+sed -e "s/\<string\>default\<\/string\>/\<string\>"$1"\<\/string\>/" "$target_file" > "$target_file.$$" && mv "$target_file.$$" "$target_file"
+
 cat ios/Tidepool/Supporting/EXShell.plist # remove this later, just testing Travis CI
