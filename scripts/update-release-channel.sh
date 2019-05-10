221302d5796a73fe88566019da406cb7a3af0977
Mark Young <mark@dufflite.com>
2019-05-10 13:57:45 -0500

    Attempt to fix Travis build

--- scripts/update-release-channel.sh
+++ scripts/update-release-channel.sh
@@ -2,21 +2,24 @@
 
 set -o nounset
 
-# Update manifests to use specified release channel rather than default
+updateReleaseChannel() {
+    sed -e "$expression" "$target_file" > "$target_file.bak" && mv "$target_file.bak" "$target_file"
+}
 
-target_file=android/app/src/main/assets/shell-app-manifest.json
-sed -e "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$1"\"/" "$target_file" > "$target_file.$$" && mv "$target_file.$$" "$target_file"
+releaseChannel=$1
 
-target_file=android/app/src/main/assets/kernel-manifest.json
-sed -e "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$1"\"/" "$target_file" > "$target_file.$$" && mv "$target_file.$$" "$target_file"
+expression="s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$releaseChannel"\"/"
+target_file="android/app/src/main/assets/shell-app-manifest.json"
+updateReleaseChannel
+target_file="android/app/src/main/assets/kernel-manifest.json"
+updateReleaseChannel
+target_file="ios/Tidepool/Supporting/shell-app-manifest.json"
+updateReleaseChannel
+target_file="ios/Tidepool/Supporting/EXShell.json"
+updateReleaseChannel
 
-target_file=ios/Tidepool/Supporting/shell-app-manifest.json
-sed -e "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$1"\"/" "$target_file" > "$target_file.$$" && mv "$target_file.$$" "$target_file"
-
-target_file=ios/Tidepool/Supporting/EXShell.json
-sed -e "s/\"releaseChannel\"\:\"default\"/\"releaseChannel\"\:\""$1"\"/" "$target_file" > "$target_file.$$" && mv "$target_file.$$" "$target_file"
-
-target_file=ios/Tidepool/Supporting/EXShell.plist
-sed -e "s/\<string\>default\<\/string\>/\<string\>"$1"\<\/string\>/" "$target_file" > "$target_file.$$" && mv "$target_file.$$" "$target_file"
+expression="s/\<string\>default\<\/string\>/\<string\>"$releaseChannel"\<\/string\>/"
+target_file="ios/Tidepool/Supporting/EXShell.plist"
+updateReleaseChannel
 
 cat ios/Tidepool/Supporting/EXShell.plist # remove this later, just testing Travis CI
