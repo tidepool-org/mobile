d7e3710ed20ca4cd46e51267ef772dbaa49b80c1
Mark Young <mark@dufflite.com>
2019-07-12 10:00:08 -0500

    Remove SVG

--- src/components/Graph/svg/GraphYAxisBGBoundaryLinesSvg.js
+++ src/components/Graph/svg/GraphYAxisBGBoundaryLinesSvg.js
@@ -1,67 +1,67 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-import { withTheme } from "glamorous-native";
-import { Svg } from "expo";
+// import React, { PureComponent } from "react";
+// import PropTypes from "prop-types";
+// import { withTheme } from "glamorous-native";
+// import { Svg } from "expo";
 
-import { ThemePropType } from "../../../prop-types/theme";
-// import Logger from "../../../models/Logger";
+// import { ThemePropType } from "../../../prop-types/theme";
+// // import Logger from "../../../models/Logger";
 
-const { Path } = Svg;
+// const { Path } = Svg;
 
-class GraphYAxisBGBoundaryLinesSvg extends PureComponent {
-  renderBGBoundaryLine(value) {
-    const {
-      theme,
-      graphFixedLayoutInfo: {
-        yAxisGlucoseHeight,
-        yAxisGlucosePixelsPerValue,
-        width,
-      },
-    } = this.props;
+// class GraphYAxisBGBoundaryLinesSvg extends PureComponent {
+//   renderBGBoundaryLine(value) {
+//     const {
+//       theme,
+//       graphFixedLayoutInfo: {
+//         yAxisGlucoseHeight,
+//         yAxisGlucosePixelsPerValue,
+//         width,
+//       },
+//     } = this.props;
 
-    const yOffset = Math.round(
-      yAxisGlucoseHeight - value * yAxisGlucosePixelsPerValue
-    );
+//     const yOffset = Math.round(
+//       yAxisGlucoseHeight - value * yAxisGlucosePixelsPerValue
+//     );
 
-    const x1 = 30;
-    const x2 = width - 4;
-    const y = yOffset;
-    const d = `M${x1} ${y} L${x2} ${y}`;
+//     const x1 = 30;
+//     const x2 = width - 4;
+//     const y = yOffset;
+//     const d = `M${x1} ${y} L${x2} ${y}`;
 
-    return (
-      <Path
-        key={value}
-        d={d}
-        stroke={theme.graphLineStrokeColor}
-        strokeDasharray="2, 5"
-        strokeLinecap="square"
-        strokeLineJoin="round"
-        strokeWidth="1.5"
-      />
-    );
-  }
+//     return (
+//       <Path
+//         key={value}
+//         d={d}
+//         stroke={theme.graphLineStrokeColor}
+//         strokeDasharray="2, 5"
+//         strokeLinecap="square"
+//         strokeLineJoin="round"
+//         strokeWidth="1.5"
+//       />
+//     );
+//   }
 
-  render() {
-    // console.log(`GraphYAxisBGBoundaryLinesSvg: render`);
+//   render() {
+//     // console.log(`GraphYAxisBGBoundaryLinesSvg: render`);
 
-    const {
-      yAxisBGBoundaryValues,
-      graphFixedLayoutInfo: { width, graphLayerHeight },
-    } = this.props;
+//     const {
+//       yAxisBGBoundaryValues,
+//       graphFixedLayoutInfo: { width, graphLayerHeight },
+//     } = this.props;
 
-    return (
-      <Svg height={graphLayerHeight} width={width}>
-        {yAxisBGBoundaryValues.map(value => this.renderBGBoundaryLine(value))}
-      </Svg>
-    );
-  }
-}
+//     return (
+//       <Svg height={graphLayerHeight} width={width}>
+//         {yAxisBGBoundaryValues.map(value => this.renderBGBoundaryLine(value))}
+//       </Svg>
+//     );
+//   }
+// }
 
-GraphYAxisBGBoundaryLinesSvg.propTypes = {
-  theme: ThemePropType.isRequired,
-  yAxisBGBoundaryValues: PropTypes.arrayOf(PropTypes.number.isRequired)
-    .isRequired,
-  graphFixedLayoutInfo: PropTypes.object.isRequired,
-};
+// GraphYAxisBGBoundaryLinesSvg.propTypes = {
+//   theme: ThemePropType.isRequired,
+//   yAxisBGBoundaryValues: PropTypes.arrayOf(PropTypes.number.isRequired)
+//     .isRequired,
+//   graphFixedLayoutInfo: PropTypes.object.isRequired,
+// };
 
-export default withTheme(GraphYAxisBGBoundaryLinesSvg);
+// export default withTheme(GraphYAxisBGBoundaryLinesSvg);
