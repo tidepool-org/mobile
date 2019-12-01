fc80f881b802851b87e7113afd2cd76d319ae2ea
Mark Young <mark@dufflite.com>
2019-11-30 19:49:37 -0600

    MOBILE-167: Remove deprecated UIWebView API usage that will prevent submission to App Store - Update to latest react-native-device-info (previous version used UIWebView) - Re-apply our patch to latest react-native-device-info NOTE: Still need to do some more work for the rest of project to remove UIWebView. Will likely just update to Expo 35 to get this

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp0815d1104db745568824ac69a998de27://10.0.1.124:80";
+  public static final String DEVELOPMENT_URL = "exp://10.0.1.124:19000";
 
 }
