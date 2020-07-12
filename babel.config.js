0c98117808bf587efde7db04df59fcae2ef12842
Mark Young <mark@dufflite.com>
2020-07-12 06:28:47 -0500

    Fix a babel issue

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,13 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { lazyImports: true }]],
-    plugins: ["transform-inline-environment-variables"],
+    plugins: [
+      [
+        "transform-inline-environment-variables",
+        {
+          include: ["ROLLBAR_POST_CLIENT_TOKEN"],
+        },
+      ],
+    ],
   };
 };
