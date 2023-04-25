0a3bddd35eb1c2e9d69668e62b13bd753d530a55
Ginny Yadav <ginny@tidepool.org>
2023-04-25 10:06:48 -0400

    use new navigation library

--- src/reducers/navigation.js
+++ src/reducers/navigation.js
@@ -1,225 +1,71 @@
-import {
-  NavigationActions,
-  StackActions,
-  DrawerActions,
-} from "react-navigation";
-import { Linking } from "react-native";
+import * as React from 'react';
+import { Linking } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Urls from '../constants/Urls';
 
-import Urls from "../constants/Urls";
-import { AppNavigator } from "../navigators/AppNavigator";
-import isRouteNameOnStack from "../navigators/isRouteNameOnStack";
-import getRouteName from "../navigators/getRouteName";
-import {
-  ADD_COMMENT_ROUTE_NAME,
-  ADD_NOTE_ROUTE_NAME,
-  DEBUG_SETTINGS_ROUTE_NAME,
-  EDIT_COMMENT_ROUTE_NAME,
-  EDIT_NOTE_ROUTE_NAME,
-  LAUNCH_ROUTE_NAME,
-  MAIN_DRAWER_ROUTE_NAME,
-  SIGN_IN_ROUTE_NAME,
-  SWITCH_PROFILE_ROUTE_NAME,
-} from "../navigators/routeNames";
-import {
-  NAVIGATE_LAUNCH,
-  NAVIGATE_HOME,
-  NAVIGATE_SIGN_IN,
-  NAVIGATE_SIGN_UP,
-  NAVIGATE_FORGOT_PASSWORD,
-  NAVIGATE_HOW_TO_UPLOAD,
-  NAVIGATE_SWITCH_PROFILE,
-  NAVIGATE_ADD_NOTE,
-  NAVIGATE_EDIT_NOTE,
-  NAVIGATE_ADD_COMMENT,
-  NAVIGATE_EDIT_COMMENT,
-  NAVIGATE_PRIVACY_AND_TERMS,
-  NAVIGATE_SUPPORT,
-  NAVIGATE_DRAWER_OPEN,
-  NAVIGATE_DRAWER_CLOSE,
-  NAVIGATE_DEBUG_SETTINGS,
-  NAVIGATE_GO_BACK,
-} from "../actions/navigation";
+import AddOrEditCommentScreen from '../screens/AddOrEditCommentScreen';
+import AddOrEditNoteScreen from '../screens/AddOrEditNoteScreen';
+import DebugSettingsScreen from '../screens/DebugSettingsScreen';
+import HomeScreen from '../screens/HomeScreen';
+import LaunchScreen from '../screens/LaunchScreen';
+import SignInScreen from '../screens/SignInScreen';
+import SwitchProfileScreen from '../screens/SwitchProfileScreen';
 
-const shouldIgnoreNextNavigate = ({ nextState, state }) => {
-  // Prevent double navigation for some routes (e.g. when tapping UI elements that cause navigation quickly)
-  // TODO: Revisit this. It's kind of hacky / fragile
-  if (nextState !== state) {
-    const { routeName } = getRouteName({
-      navigation: nextState,
-    });
-    const nextRouteName = routeName;
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-    const routeNames = [
-      SWITCH_PROFILE_ROUTE_NAME,
-      ADD_NOTE_ROUTE_NAME,
-      EDIT_NOTE_ROUTE_NAME,
-      ADD_COMMENT_ROUTE_NAME,
-      EDIT_COMMENT_ROUTE_NAME,
-    ];
-    if (
-      nextRouteName &&
-      routeNames.includes(nextRouteName) &&
-      isRouteNameOnStack({
-        routeName: nextRouteName,
-        navigationState: state,
-      })
-    ) {
-      return true;
-    }
-  }
-
-  return false;
-};
-
-const signInAction = AppNavigator.router.getActionForPathAndParams(
-  SIGN_IN_ROUTE_NAME
-);
-const SignInActionState = AppNavigator.router.getStateForAction(signInAction);
-const initialState = AppNavigator.router.getStateForAction(
-  signInAction,
-  SignInActionState
-);
-
-function navigation(state = initialState, action) {
-  let nextState;
-
-  switch (action.type) {
-    case NAVIGATE_LAUNCH:
-      nextState = AppNavigator.router.getStateForAction(
-        StackActions.reset({
-          index: 0,
-          key: null,
-          actions: [
-            NavigationActions.navigate({ routeName: LAUNCH_ROUTE_NAME }),
-          ],
-        }),
-        state
-      );
-      break;
-    case NAVIGATE_HOME:
-      nextState = AppNavigator.router.getStateForAction(
-        StackActions.reset({
-          index: 0,
-          key: null,
-          actions: [
-            NavigationActions.navigate({ routeName: MAIN_DRAWER_ROUTE_NAME }),
-          ],
-        }),
-        state
-      );
-      break;
-    case NAVIGATE_SIGN_IN:
-      nextState = AppNavigator.router.getStateForAction(
-        StackActions.reset({
-          index: 0,
-          key: null,
-          actions: [
-            NavigationActions.navigate({ routeName: SIGN_IN_ROUTE_NAME }),
-          ],
-        }),
-        state
-      );
-      break;
-    case NAVIGATE_SWITCH_PROFILE:
-      nextState = AppNavigator.router.getStateForAction(
-        NavigationActions.navigate({ routeName: SWITCH_PROFILE_ROUTE_NAME }),
-        state
-      );
-      break;
-    case NAVIGATE_ADD_NOTE:
-      nextState = AppNavigator.router.getStateForAction(
-        NavigationActions.navigate({
-          routeName: ADD_NOTE_ROUTE_NAME,
-          params: { note: action.payload.note },
-        }),
-        state
-      );
-      break;
-    case NAVIGATE_EDIT_NOTE:
-      nextState = AppNavigator.router.getStateForAction(
-        NavigationActions.navigate({
-          routeName: EDIT_NOTE_ROUTE_NAME,
-          params: { note: action.payload.note },
-        }),
-        state
-      );
-      break;
-    case NAVIGATE_ADD_COMMENT:
-      nextState = AppNavigator.router.getStateForAction(
-        NavigationActions.navigate({
-          routeName: ADD_COMMENT_ROUTE_NAME,
-          params: {
-            note: action.payload.note,
-            commentsFetchData: action.payload.commentsFetchData,
-          },
-        }),
-        state
-      );
-      break;
-    case NAVIGATE_EDIT_COMMENT:
-      nextState = AppNavigator.router.getStateForAction(
-        NavigationActions.navigate({
-          routeName: EDIT_COMMENT_ROUTE_NAME,
-          params: {
-            note: action.payload.note,
-            comment: action.payload.comment,
-            commentsFetchData: action.payload.commentsFetchData,
-            graphFetchData: action.payload.graphFetchData,
-          },
-        }),
-        state
-      );
-      break;
-    case NAVIGATE_PRIVACY_AND_TERMS:
-      Linking.openURL(Urls.privacyAndTerms);
-      break;
-    case NAVIGATE_SUPPORT:
-      Linking.openURL(Urls.support);
-      break;
-    case NAVIGATE_FORGOT_PASSWORD:
-      Linking.openURL(Urls.forgotPassword);
-      break;
-    case NAVIGATE_SIGN_UP:
-      Linking.openURL(Urls.signUp);
-      break;
-    case NAVIGATE_HOW_TO_UPLOAD:
-      Linking.openURL(Urls.howToUpload);
-      break;
-    case NAVIGATE_DRAWER_OPEN:
-      nextState = AppNavigator.router.getStateForAction(
-        DrawerActions.openDrawer(),
-        state
-      );
-      break;
-    case NAVIGATE_DRAWER_CLOSE:
-      nextState = AppNavigator.router.getStateForAction(
-        DrawerActions.closeDrawer(),
-        state
-      );
-      break;
-    case NAVIGATE_DEBUG_SETTINGS:
-      nextState = AppNavigator.router.getStateForAction(
-        NavigationActions.navigate({ routeName: DEBUG_SETTINGS_ROUTE_NAME }),
-        state
-      );
-      break;
-    case NAVIGATE_GO_BACK:
-      nextState = AppNavigator.router.getStateForAction(
-        NavigationActions.back(),
-        state
-      );
-      break;
-    default:
-      nextState = AppNavigator.router.getStateForAction(action, state);
-      break;
-  }
-
-  nextState = nextState || state;
-  if (shouldIgnoreNextNavigate({ nextState, state })) {
-    nextState = state;
-  }
-
-  return nextState;
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Launch" component={LaunchScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="SignIn" component={SignInScreen} />
+      
+    </Stack.Navigator>
+  );
 }
 
-export default navigation;
+function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+      <Drawer.Screen name="Root" component={RootStack} />
+      <Drawer.Screen name="SwitchProfile" component={SwitchProfileScreen} />
+      <Drawer.Screen name="AddNote" component={AddOrEditNoteScreen} />
+      <Drawer.Screen name="EditNote" component={AddOrEditNoteScreen} />
+      <Drawer.Screen name="AddComment" component={AddOrEditCommentScreen} />
+      <Drawer.Screen name="EditComment" component={AddOrEditCommentScreen} />
+      <Drawer.Screen name="DebugSettings" component={DebugSettingsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigation;
+
+// Navigation functions
+export function navigateToURL(url) {
+  Linking.openURL(url);
+}
+
+export function navigateToPrivacyAndTerms() {
+  navigateToURL(Urls.privacyAndTerms);
+}
+
+export function navigateToSupport() {
+  navigateToURL(Urls.support);
+}
+
+export function navigateToForgotPassword() {
+  navigateToURL(Urls.forgotPassword);
+}
+
+export function navigateToSignUp() {
+  navigateToURL(Urls.signUp);
+}
+
+export function navigateToHowToUpload() {
+  navigateToURL(Urls.howToUpload);
+}
